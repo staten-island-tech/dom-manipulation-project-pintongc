0e478014c446050a93ec2dcabcce1e79e4dfae59
pintongc <pintongc@nycstudents.net>
2023-10-17 09:23:12 -0400

    3

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const form = document.querySelector("#form")
-console.log(form)
+const formfirst = document.querySelector("#form")
+const formlast = document.querySelector("#form1")
+const formimg = document.querySelector("#form2")
+
+console.log(formlast)
 
 const DOMSelectors = {
-    form: document.querySelector("#form"),
+    formfirst: document.querySelector("#form"),
+    formlast: document.querySelector("#form1"),
+    formimg: document.querySelector("#form2"),
     firstName: document.querySelector(".first-name"),
     lastName: document.querySelector(".last-name"),
     profilePicture: document.querySelector(".profile-picture"),
     h2s: document.querySelectorAll("h2"),
-    imgs: document.querySelector("img"),
+    h3s: document.querySelectorAll("h3"),
+    imgs: document.querySelectorAll("img"),
     text: document.querySelector(".profile"),
 };
 
 firstName();
 function firstName() {
-    DOMSelectors.form.addEventListener("submit", function (event) {
+    DOMSelectors.formfirst.addEventListener("submit", function (event) {
         event.preventDefault();
         console.log(DOMSelectors.firstName.value);
         DOMSelectors.h2s.forEach((el) => el.textContent = DOMSelectors.firstName.value
+        );
+    });
+};
+
+lastName();
+function lastName() {
+    DOMSelectors.formlast.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log(DOMSelectors.lastName.value);
+        DOMSelectors.h3s.forEach((el) => el.textContent = DOMSelectors.lastName.value
+        );
+    });
+};
+
+profilePicture();
+function profilePicture() {
+    DOMSelectors.formimg.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log(DOMSelectors.profilePicture.value);
+        DOMSelectors.imgs.forEach((el) => el.textContent = DOMSelectors.profilePicture.value
         );
     });
 };
