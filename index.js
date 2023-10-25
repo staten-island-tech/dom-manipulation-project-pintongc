901fb35360ec266da05761ba1edd99faaccda981
pintongc <pintongc@nycstudents.net>
2023-10-25 09:24:38 -0400

    3

--- index.js
+++ index.js
@@ -48,6 +48,10 @@ function profilePicture() {
     });
 };
 
+DOMSelectors.cardTitle.insertAdjacentHTML("beforeend", '<img src="https://images..com/photo-1683009427598-9c21a169f98f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">')
+
+
+
 
 
 
