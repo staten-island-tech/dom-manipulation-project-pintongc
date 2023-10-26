ef6469ed17a93de6748c97529ad4d956e88974a6
pintongc <pintongc@nycstudents.net>
2023-10-26 09:24:33 -0400

    5

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const form = document.querySelector("#form")
+const formfirst = document.querySelector("#form")
+const formlast = document.querySelector("#form1")
+const formimg = document.querySelector("#form2")
 
 console.log(formlast)
 
@@ -14,35 +16,56 @@ const DOMSelectors = {
     imgs: document.querySelectorAll("img"),
     text: document.querySelector(".profile"),
     cardTitle: document.querySelector(".card-title"),
+    reset: document.querySelector("#Reset"),
+    gallery: document.querySelector(".gallery"),
+};
+
+function firstName() {
+    DOMSelectors.formfirst.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log(DOMSelectors.firstName.value);
+        DOMSelectors.h2s.forEach((el) => el.textContent = DOMSelectors.firstName.value;
+        );
+    });
 };
 
 const firstName = DOMSelectors.firstName.value;
 const lastName = DOMSelectors.lastName.value;
 const profilePicture = DOMSelectors.profilePicture.value;
 
+DOMSelectors.firstName.insertAdjacentHTML("beforeend", '<div class="card" $(DOMSelectors.firstName.value) </div>')
+
 function createProfile() {
-    DOMSelectors.formfirst.addEventListener('submit', function(event) {
-      event.preventDefault();
 
   const firstName = document.querySelector('#first-name').value;
-      const lastName = document.querySelector('#last-name').value;
-      const profilePicture = document.querySelector('#profile-picture').value;
+  const lastName = document.querySelector('#last-name').value;
+  const profilePicture = document.querySelector('#profile-picture').value;
   
-      const object = {
-        firstName,
-        lastName,
-        profilePicture,
+    const object = {
+      firstName,
+      lastName,
+      profilePicture,
       };
+
+    injectProfileIntoDOM(object);
   
-      injectProfileIntoDOM(object);
-  
-      clearInputFields();
-    });
-  }
-  
+    clearInputFields();
+    };
+
+function injectProfileIntoDOM() {
+
+};
+
+function clearInputFields() {
+  const reset = document.querySelector("#reset");
+  reset.addEventListener("click", () => {
+    document.location.reload();
+  });
+};
+
 DOMSelectors.cardTitle.insertAdjacentHTML("beforeend", '<img src="https://images..com/photo-1683009427598-9c21a169f98f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">')
 
-
+const insert = DOMSelectors.querySelector
 
 
 
