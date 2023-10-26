91f7851fab61038add0c1b31ee3662fab7df1585
pintongc <pintongc@nycstudents.net>
2023-10-26 08:42:22 -0400

    4

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const formfirst = document.querySelector("#form")
-const formlast = document.querySelector("#form1")
-const formimg = document.querySelector("#form2")
+const form = document.querySelector("#form")
 
 console.log(formlast)
 
@@ -18,36 +16,30 @@ const DOMSelectors = {
     cardTitle: document.querySelector(".card-title"),
 };
 
-firstName();
-function firstName() {
-    DOMSelectors.formfirst.addEventListener("submit", function (event) {
-        event.preventDefault();
-        console.log(DOMSelectors.firstName.value);
-        DOMSelectors.h2s.forEach((el) => el.textContent = DOMSelectors.firstName.value
-        );
-    });
-};
+const firstName = DOMSelectors.firstName.value;
+const lastName = DOMSelectors.lastName.value;
+const profilePicture = DOMSelectors.profilePicture.value;
 
-lastName();
-function lastName() {
-    DOMSelectors.formlast.addEventListener("submit", function (event) {
-        event.preventDefault();
-        console.log(DOMSelectors.lastName.value);
-        DOMSelectors.h3s.forEach((el) => el.textContent = DOMSelectors.lastName.value
-        );
-    });
-};
+function createProfile() {
+    DOMSelectors.formfirst.addEventListener('submit', function(event) {
+      event.preventDefault();
 
-profilePicture();
-function profilePicture() {
-    DOMSelectors.formimg.addEventListener("submit", function (event) {
-        event.preventDefault();
-        console.log(DOMSelectors.profilePicture.value);
-        DOMSelectors.imgs.forEach((el) => el.textContent = DOMSelectors.profilePicture.value
-        );
+  const firstName = document.querySelector('#first-name').value;
+      const lastName = document.querySelector('#last-name').value;
+      const profilePicture = document.querySelector('#profile-picture').value;
+  
+      const object = {
+        firstName,
+        lastName,
+        profilePicture,
+      };
+  
+      injectProfileIntoDOM(object);
+  
+      clearInputFields();
     });
-};
-
+  }
+  
 DOMSelectors.cardTitle.insertAdjacentHTML("beforeend", '<img src="https://images..com/photo-1683009427598-9c21a169f98f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">')
 
 
